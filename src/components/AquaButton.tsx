fce55c880fb7fcea47a9a9cb3d7a974b630fa8db
deepikaprasadr <deepikaprasad.tsm@gmail.com>
2025-07-30 22:49:02 -0400

    TSXMantineReact

---
+++ src/components/AquaButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mantine/core';
+
+function AquaButton() {
+  return (
+    <Button style={{ backgroundColor: 'aqua', color: 'black' }}>
+      Aqua Button
+    </Button>
+  );
+}
+
+export default AquaButton;
